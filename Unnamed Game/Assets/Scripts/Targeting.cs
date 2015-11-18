f5473ee5147ec9e60b45a445864baab7712008af
marcusthegreat <marcdfiot@gmail.com>
2015-11-18 12:54:07 -0600

    Update Targeting
    Fixed syntax errors

--- Unnamed Game/Assets/Scripts/Targeting.cs
+++ Unnamed Game/Assets/Scripts/Targeting.cs
@@ -5,11 +5,15 @@ public class Targeting : MonoBehaviour {
 
 //Not Run-able ATM concept
 
-	float[] TargetManager (int x, int y){  //input xy distance from launch
-		float theta = atand((2*y)/(x/2))); // Angle to theoretical arc peak so projectivle has -vel. when hitting taget // ERROR-whats arc tan?
-		if (theta = 0){
-			theta = .0000000001; // Needs to be divided by later can't be 0 w/o error // ERROR-curently a hotfix
+	float[] TargetManager (int x, int y){  // Input xy distance from launch
+		float theta = arctan((2*y)/(x/2))); // Angle to theoretical arc peak so projectivle has -vel. when hitting taget // ERROR-whats arc tan?
+		if (theta == 0){
+			theta = .00000001; // Needs to be divided by later can't be 0 w/o error // ERROR-curently a hotfix
 		}
+		if (theta == 180){
+			theta = 180.0000001;
+		}
+		
 		float xVi = sqrt((G*x)/(sin(2*theta))); // Initial Velocity in x-axis // ERROR-sqrt & sin functions in c#? replace G with game gravity constant
 		float yVi = sqrt((2*G*y)/(sin(theta)^2)); // Initial Velocity in y-axis
 		float totalVi = sqrt(xVi^2 + yVi^2); // Total Velocity along theta
