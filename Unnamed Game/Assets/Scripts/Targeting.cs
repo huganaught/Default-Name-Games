1edfe8998fc51c16537af1633259607230c023c5
marcusthegreat <marcdfiot@gmail.com>
2015-11-18 12:44:44 -0600

    Added Targeting Concept
    Initial script

---
+++ Unnamed Game/Assets/Scripts/Targeting.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class Targeting : MonoBehaviour {
+
+//Not Run-able ATM conept
+
+	float[] TargetManager (int x, int y){  //input xy distance from launch
+		float theta = atand((2*y)/(x/2))); // Angle to theoretical arc peak so projectivle has -vel. when hitting taget // ERROR-whats arc tan?
+		if (theta = 0){
+			theta = .0000000001; // Needs to be divided by later can't be 0 w/o error // ERROR-curently a hotfix
+		}
+		float xVi = sqrt((G*x)/(sin(2*theta))); // Initial Velocity in x-axis // ERROR-sqrt & sin functions in c#? replace G with game gravity constant
+		float yVi = sqrt((2*G*y)/(sin(theta)^2)); // Initial Velocity in y-axis
+		float totalVi = sqrt(xVi^2 + yVi^2); // Total Velocity along theta
+		return {totalVi, theta};
+		/* // If needed... without physics engine; time independent
+		x = xV + x; // Calculates next position x-axis constant vel. can change if (wind) applied like gravity
+		xV = xVi + someForce;
+		y = yV + y;
+		yV = yVi - G;
+		
+		object(x,y); // Where object would now be
+		*/
+	}
+
